b891bd7e47262cf5145821f5ca8f61c59cbb1e4e
kingsley-akaehomen <akaehomenkingsley@gmail.com>
2024-04-26 22:50:57 +0100

    post controllers

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,11 +1,13 @@
 import express from "express";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getPosts)
 router.get("/:id", getPost)
-router.post("/", addPost)
-router.put("/", updatePost)
-router.delete("/", deletePost)
+router.post("/", verifyToken, addPost)
+router.put("/", verifyToken, updatePost)
+router.delete("/", verifyToken, deletePost)
 
 export default router;
